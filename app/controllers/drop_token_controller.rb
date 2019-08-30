61ee08821eefe2951a8f80f5156601b6fc94e8fa
Dominic Baratta <dbaratta@buffalo.edu>
2019-08-29 22:41:04 -0700

    Adding skeleton controllers for DropToken games and moves. Also adding them to the application routes

---
+++ app/controllers/drop_token_controller.rb
@@ -0,0 +1,8 @@
+class DropTokenController < ApplicationController
+  # GET /drop_token
+  def index; end
+
+  def create; end
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # To see available routes run 'rails routes'
+
+  # Provide default routes (get, post, patch, delete, etc) for the DropToken controller.
+  resources :drop_token do
+    resources :moves, only: %i[index show]
+  end
 end
